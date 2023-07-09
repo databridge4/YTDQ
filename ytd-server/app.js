a007ec61e7e76ad51cc4bb12b57193998f58675d
databridge4 <133959894+databridge4@users.noreply.github.com>
2023-07-09 19:44:04 +0300

    Update app.js

--- ytd-server/app.js
+++ ytd-server/app.js
@@ -9,10 +9,12 @@ const port = process.env.PORT || 3000;
 const youtubeTranscript = require("./Routes/youtebeTranscript");
 const youtubeDownloader = require("./Routes/youtubeFragmentDownloader");
 const youtubeQuality = require("./Routes/youtubeQuality");
+const youtubeDuration = require("./Routes/YoutubeDuration");
 
 app.use("/transcript", youtubeTranscript);
 app.use("/videoDownload", youtubeDownloader);
 app.use("/quality", youtubeQuality);
+app.use("/duration", youtubeDuration);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
